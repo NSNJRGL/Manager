a5710688e9bddb62c679f5c5fbde44beb103f2e9
Nasaakaa <nasaa1997@gmail.com>
2020-05-15 12:16:15 +0800

    rendered map view

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,20 +1,55 @@
-import React from 'react';
-import {StyleSheet} from 'react-native';
+import React, {useEffect} from 'react';
+import {StyleSheet, View} from 'react-native';
 import {Layout} from '@ui-kitten/components';
+import MapView from 'react-native-maps';
 
 import CustomTopNavigation from '../components/CustomTopNavigation';
 
 const MapScreen = () => {
+  useEffect(() => {}, []);
+
   return (
-    <Layout style={styles.container}>
+    <Layout style={styles.mainContainer}>
       <CustomTopNavigation title="Байршил" />
+
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          region={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+          showsMyLocationButton={true}
+          showsUserLocation={true}>
+          <MapView.Marker
+            coordinate={{
+              latitude: 37.78825,
+              longitude: -122.4324,
+            }}
+            title={'Your Location'}
+            draggable
+          />
+        </MapView>
+      </View>
     </Layout>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     minHeight: '100%',
+  },
+  container: {
+    height: '100%',
+    width: '100%',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    flex: 1,
+    ...StyleSheet.absoluteFillObject,
   },
 });
 
